2c7caa301b62d15abd75e1ad80606da375e75fb6
MakeMake23 <andy.ledesma.garcia@gmail.com>
2025-03-31 14:44:19 +0400

    build: use fixed Solidity v0.8.26

--- src/mock/MultisigAttacker.sol
+++ src/mock/MultisigAttacker.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.28;
+pragma solidity 0.8.26;
 
 interface IMultisigCaller {
     function submitTransaction(address to, uint256 value, bytes memory data) external;
